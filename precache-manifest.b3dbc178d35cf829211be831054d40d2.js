8cddbc2dcd0f9088d959d95f95ef37e3a45786b6
Joris Buchou <joris.buchou@gmail.com>
2019-01-03 18:56:57 -0800

    Updates

---
+++ precache-manifest.b3dbc178d35cf829211be831054d40d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb5ee8d8be4b2facf42a",
+    "url": "/pro-news/static/js/runtime~main.eb5ee8d8.js"
+  },
+  {
+    "revision": "9858b7cc961146e55601",
+    "url": "/pro-news/static/js/main.9858b7cc.chunk.js"
+  },
+  {
+    "revision": "f34b990cd630eb595afc",
+    "url": "/pro-news/static/js/1.f34b990c.chunk.js"
+  },
+  {
+    "revision": "9858b7cc961146e55601",
+    "url": "/pro-news/static/css/main.21446925.chunk.css"
+  },
+  {
+    "revision": "0af4c0e5d4464c2e8eef192f5ac2edd5",
+    "url": "/pro-news/index.html"
+  }
+];
