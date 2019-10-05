90828e3aa930cc01c49902c67f2794a2f44c84c1
Joris Buchou <joris.buchou@gmail.com>
2019-10-04 18:11:22 -0700

    Updates

---
+++ precache-manifest.61af36e6d25b8a21f571316a0eb675ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686b2167ce1022e8918d5c960f9d8489",
+    "url": "/pro-news/index.html"
+  },
+  {
+    "revision": "f9c6394d323984e5d57c",
+    "url": "/pro-news/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "d4c4af80cb96b2af41dd",
+    "url": "/pro-news/static/css/main.31d8ec46.chunk.css"
+  },
+  {
+    "revision": "f9c6394d323984e5d57c",
+    "url": "/pro-news/static/js/2.1c2d1681.chunk.js"
+  },
+  {
+    "revision": "d4c4af80cb96b2af41dd",
+    "url": "/pro-news/static/js/main.6f7a99d8.chunk.js"
+  },
+  {
+    "revision": "3dd679911b4de39ecbb6",
+    "url": "/pro-news/static/js/runtime-main.d0a3d73d.js"
+  }
+]);
